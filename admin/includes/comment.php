3218640a223d3d9219042c9c71c4287d13889ad7
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2023-05-22 13:22:34 +0200

    Update

---
+++ admin/includes/comment.php
@@ -0,0 +1,26 @@
+<?php
+
+class Comment extends Db_objext {
+
+    protected static $db_table = "comments";
+    protected static $db_table_fields = array('id' , 'photo_id' , 'author' , 'body');
+    public $id;
+    public $photo_id;
+    public $author;
+    public $body;
+    
+
+    public static function create_comment($photo_id, $author="Jhon", $body){
+        if (!empty($photo_id) && !empty($author) && !empty($body)) {
+            $comment = new Comment;
+            $comment->photo_id = (int)$photo_id;
+            $comment->author = $author;
+            $comment->body = $body;
+            return $comment;
+        }
+    }
+} // end Comment class
+
+
+
+?>
