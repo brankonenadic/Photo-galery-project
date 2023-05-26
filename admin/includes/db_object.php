46c5fd7ffe1958a8254f06f00f1df811d8115737
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2023-05-26 12:00:20 +0200

    Update

--- admin/includes/db_object.php
+++ admin/includes/db_object.php
@@ -132,7 +132,13 @@ class Db_objext {
 
     }
 
-
+    public static function count_all(){
+        global $database;
+        $sql = "SELECT COUNT(*) FROM " . static::$db_table;
+        $result_set = $database->query($sql);
+        $row = mysqli_fetch_array($result_set);
+        return array_shift($row);
+    }
 
 
 
