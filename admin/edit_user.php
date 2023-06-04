ce0f03d40b55761892f9ae89bed5004409e81816
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2023-06-04 13:56:44 +0200

    Update

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -1,104 +1,46 @@
-
 <?php include("includes/header.php"); ?>
-
 <?php include("includes/photo_library_modal.php"); ?>
-
 <?php if(!$session->is_signed_in()) {redirect("login.php");} ?>
-
 <?php 
-
-
-
 if(empty($_GET['id'])) {
-
 redirect("users.php");
-
 }
-
 $user = User::find_by_id($_GET['id']);
-
 
 if(isset($_POST['update'])) {
 
+    if($user) {
+        $user->username = $_POST['username'];
+        $user->first_name =$_POST['first_name'];
+        $user->last_name =$_POST['last_name'];
+        $user->password =$_POST['password'];
 
-if($user) {
-
-
-$user->username = $_POST['username'];
-$user->first_name =$_POST['first_name'];
-$user->last_name =$_POST['last_name'];
-$user->password =$_POST['password'];
-
-if(empty($_FILES['user_img'])) {
-
-$user->save();
-redirect("users.php");
-$session->message("The user has been updated");
-
-} else {
-
-$user->set_file($_FILES['user_img']);
-$user->upload_photo();
-$user->save();
-$session->message("The user has been updated");
-
-// redirect("edit_user.php?id={$user->id}");
-redirect("users.php");
-
-
-
+        if(empty($_FILES['user_img'])) {
+            $user->save();
+            redirect("users.php");
+            $session->message("The user has been updated");
+        } else {
+            $user->set_file($_FILES['user_img']);
+            $user->upload_photo();
+            $user->save();
+            $session->message("The user has been updated");
+            // redirect("edit_user.php?id={$user->id}");
+            redirect("users.php");
+        }
+    }
 }
-
-
-
-
-
-
-
-}
-
-
-
-}
-
-
-
-
 // $users = user::find_all();
-
  ?>
-
-
-
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <!-- Brand and toggle get grouped for better mobile display -->
-
-
-
         <?php include("includes/top_nav.php") ?>
-
-
-
-
-
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-           
-
-    
         <?php include("includes/side_nav.php"); ?>
-
-
-
-
             <!-- /.navbar-collapse -->
         </nav>
-
         <div id="page-wrapper">
-
-
             <div class="container-fluid">
-
                 <!-- Page Heading -->
                 <div class="row">
                     <div class="col-lg-12">
@@ -106,93 +48,43 @@ redirect("users.php");
                             users
                             <small>Subheading</small>
                         </h1>
-
-                      <div class="col-md-6 user_img_box">
-                          
+                      <div class="col-md-6 user_img_box">     
                          <a href="#" data-toggle="modal" data-target="#photo-library"><img class="img-responsive admin-user-tumbnail" src="<?php echo $user->image_path_and_placeholder(); ?>" alt=""></a>
-
-
-
                       </div>
-
-
-                    <form action="" method="post" enctype="multipart/form-data">
-
-  
-
+                <form action="" method="post" enctype="multipart/form-data">
 
                         <div class="col-md-6">
-
-                           <div class="form-group">
-                           
-                            <input type="file" name="user_img">
-                               
+                           <div class="form-group">                   
+                                <input type="file" name="user_img">                          
                            </div>
-
-
                            <div class="form-group">
-                            <label for="username">Username</label>
-                            <input type="text" name="username" class="form-control" value="<?php echo $user->username; ?>" >
-                               
+                                <label for="username">Username</label>
+                                <input type="text" name="username" class="form-control" value="<?php echo $user->username; ?>" >     
                            </div>
-
-
                             <div class="form-group">
                                 <label for="first name">First Name</label>
-                            <input type="text" name="first_name" class="form-control" value="<?php echo $user->first_name; ?>"  >
-                               
+                                <input type="text" name="first_name" class="form-control" value="<?php echo $user->first_name; ?>"  >
                            </div>
-
                             <div class="form-group">
                                 <label for="last name">Last Name</label>
-                            <input type="text" name="last_name" class="form-control" value="<?php echo $user->last_name; ?>"  >
-                               
+                                <input type="text" name="last_name" class="form-control" value="<?php echo $user->last_name; ?>"  >
                            </div>
-
-
                             <div class="form-group">
                                 <label for="password">Password</label>
-                            <input type="password" name="password" class="form-control" value="<?php echo $user->password; ?>" >
-                               
+                                <input type="password" name="password" class="form-control" value="<?php echo $user->password; ?>" >
                            </div>
-
                             <div class="form-group">
-
-                            <a id="user-id" class="btn btn-danger" href="delete_user.php?id=<?php echo $user->id; ?>">Delete</a>
-
-                            <input type="submit" name="update" class="btn btn-primary pull-right" value="Update" >
-                               
+                                <a id="user-id" class="btn btn-danger" href="delete_user.php?id=<?php echo $user->id; ?>">Delete</a>
+                                <input type="submit" name="update" class="btn btn-primary pull-right" value="Update" >
                            </div>
-
-
-                            
-
                         </div>
 
-
-
-                      
-
-            </form>
-
-
-
-
-
-
-
-
-
-
-                        
+            </form>         
                     </div>
                 </div>
                 <!-- /.row -->
-
             </div>
             <!-- /.container-fluid -->
-
         </div>
         <!-- /#page-wrapper -->
-
   <?php include("includes/footer.php"); ?>
