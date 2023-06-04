5880682c6aaaf06595694a79f4a50243cd70b265
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2023-06-04 14:58:42 +0200

    Update

--- admin/includes/photo_library_modal.php
+++ admin/includes/photo_library_modal.php
@@ -1,14 +1,5 @@
 <?php require_once("init.php"); ?>
-
-<?php 
-
-$photos = Photo::find_all();
-
-
- ?>
-
-
-
+<?php $photos = Photo::find_all(); ?>
 <div class="modal fade" id="photo-library">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -19,30 +10,21 @@ $photos = Photo::find_all();
       <div class="modal-body">
           <div class="col-md-9">
              <div class="thumbnails row">
-            
                 <!-- PHP LOOP HERE CODE HERE-->
-
                 <?php foreach($photos as $photo): ?>
-                
-               <div class="col-xs-2">
-                 <a role="checkbox" aria-checked="false" tabindex="0" id="" href="#" class="thumbnail">
-                   <img class="modal_thumbnails img-responsive" src="<?php echo $photo->picture_path(); ?>" data="<?php echo $photo->id; ?>">
-                 </a>
-                  <div class="photo-id hidden"></div>
-                  
-               </div>
-
-             <?php endforeach; ?>
-
+                  <div class="col-xs-2">
+                    <a role="checkbox" aria-checked="false" tabindex="0" id="" href="#" class="thumbnail">
+                      <img class="modal_thumbnails img-responsive" src="<?php echo $photo->picture_path(); ?>" data="<?php echo $photo->id; ?>">
+                    </a>
+                      <div class="photo-id hidden"></div>
+                  </div>
+              <?php endforeach; ?>
                     <!-- PHP LOOP HERE CODE HERE-->
-
              </div>
           </div><!--col-md-9 -->
-
   <div class="col-md-3">
     <div id="modal_sidebar"></div>
   </div>
-
    </div><!--Modal Body-->
       <div class="modal-footer">
         <div class="row">
